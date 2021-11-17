2e1e1b9ab8d0974f681a459e4f8bd40cc2c32fb7
fairyburnout <emilyjeandryer@gmail.com>
2021-11-17 11:09:37 -0600

    Fully functional with prototype PLUS function headers added. Circuit moved to P1, triggers response on servo (P0). Radio signaling still needs to be added but works for prototype step.

--- MicroBitA.js
+++ MicroBitA.js
@@ -3,20 +3,28 @@
  * MicroBit A
  */
 
-// sets radio group (to be used later)
+/**  Initializing setup: 
+ *      set radio group to 200 
+ *      Initialize servo angle to 0 
+ *      Show checkmark icon on LEDs
+ * */ 
  radio.setGroup(200)
-
- // initializes servo to 0 degrees
  servos.P0.setAngle(0)
+ basic.showIcon(IconNames.Yes)
 
+/** 
+ * Forever function is always looking out for a digital read on P1 or if A button is pressed.
+ *  if this happens, it will sitch the servo angle to 180 and switch LEDs to a happy face
+ *  Alternatively, if B button is pressed or the digital read = 1, angle will switch back to 0 degrees 
+ */
  basic.forever(function () {
-     // if button A is pressed or a digital read is recieved on P0, set angle to 180 degrees
-    if (pins.digitalReadPin(DigitalPin.P0) == 1 || input.buttonIsPressed(Button.A)) {
+     if (pins.digitalReadPin(DigitalPin.P1) == 0 || input.buttonIsPressed(Button.A)) {
          servos.P0.setAngle(180)
+         basic.showIcon(IconNames.Happy)
      }
-     // if button B is pushed, reset to 0 degrees
-     if (input.buttonIsPressed(Button.B)) {
+     if (input.buttonIsPressed(Button.B) || pins.digitalReadPin(DigitalPin.P1) == 1) {
          servos.P0.setAngle(0)
+         basic.showIcon(IconNames.Surprised)
      }
  })
  
