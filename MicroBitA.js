d3e8cb9103460e2682055a18b4e15771a28758fa
fairyburnout <emilyjeandryer@gmail.com>
2021-11-22 14:09:37 -0600

    Radio connections with strings - sending "close" or "far" based on circuitry and recieving "wolf" or "grandma" to manipulate servo

--- MicroBitA.js
+++ MicroBitA.js
@@ -12,20 +12,35 @@
  servos.P0.setAngle(0)
  basic.showIcon(IconNames.Yes)
 
-/** 
- * Forever function is always looking out for a digital read on P1 or if A button is pressed.
- *  if this happens, it will sitch the servo angle to 180 and switch LEDs to a happy face
- *  Alternatively, if B button is pressed or the digital read = 1, angle will switch back to 0 degrees 
+/** Function is looking out for strings via radio signals from Microbit B
+ *      If "wolf" is recieved, set servo to 180 degrees (upset!)
+ *      If "grandma" is recieved, set servo to 0 degrees (happy!)
+ */ 
+ radio.onReceivedString(function (receivedString) {
+    if (receivedString == "wolf") {
+        servos.P0.setAngle(180)
+    }
+    if (receivedString == "grandma") {
+        servos.P0.setAngle(0)
+    }
+})
+
+
+/** Forever function is connected to buttons and circuitry
+ *      If button A is pressed OR digital read on pin 1 = 0, 
+ *          send string "far" and happy LEDs
+ *      If button B is pressed OR digital read on  pin 1 = 1,
+ *          send string "close" and suprised LEDs
  */
- basic.forever(function () {
-     if (pins.digitalReadPin(DigitalPin.P1) == 0 || input.buttonIsPressed(Button.A)) {
-         servos.P0.setAngle(180)
-         basic.showIcon(IconNames.Happy)
-     }
-     if (input.buttonIsPressed(Button.B) || pins.digitalReadPin(DigitalPin.P1) == 1) {
-         servos.P0.setAngle(0)
-         basic.showIcon(IconNames.Surprised)
-     }
- })
- 
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) || pins.digitalReadPin(DigitalPin.P1) == 0) {
+        radio.sendString("far")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (input.buttonIsPressed(Button.B) || pins.digitalReadPin(DigitalPin.P1) == 1) {
+        radio.sendString("close")
+        basic.showIcon(IconNames.Surprised)
+    }
+})
+
  
