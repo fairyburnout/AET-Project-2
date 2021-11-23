a8cde50ea84e3f6c1b9168ad30706105f6bdd2db
gremil <ee.weller@yahoo.com>
2021-11-23 11:28:30 -0600

    changed to include 2 pins

--- MicrobitB.js
+++ MicrobitB.js
@@ -17,10 +17,10 @@
   */
  radio.onReceivedString(function (receivedString) {
     if (receivedString == "far") {
-        servos.P0.setAngle(90)
+        servos.P1.setAngle(90)
     }
     if (receivedString == "close") {
-        servos.P0.setAngle(0)
+        servos.P1.setAngle(0)
     }
 })
 
